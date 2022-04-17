c3fbf01959f2572eeb3eb5ecae63078aaeae2744
ziauljbbc <ziaul.huda@jbbc.co.jp>
2022-04-17 14:36:43 +0600

    index page meta title changed, test image removed

--- pages/msf.js
+++ pages/msf.js
@@ -8,7 +8,6 @@ export default function FirstPost() {
         <title>BG MSF</title>
       </Head>
       <h1>BG MSF</h1>
-      <img src="/images/profile.jpg" alt="Your Name" />
       <h2>
         <Link href="/">
           <a>Back to home</a>
